e7f1997b13eb765d0caba262ec165e43b84bbf86
Михаил Дарьин <darin.m@tvzavr.ru>
2018-04-13 19:37:15 +0300

    наполнен README и доработан фильт, заменяются разделители

--- categorize.pl
+++ categorize.pl
@@ -11,15 +11,18 @@ print @ARGV . "\n";
 die "usage categorize inputfile [outputfile]\n\n"
 	if @ARGV < 1;
 
-warn "ouput file is not specified. Default output fileaname will be used"
+warn "[WARN] Ouput file is not specified. Default output fileaname will be used"
 	if @ARGV < 2;
 
 my $input_filename = shift @ARGV;
 my $output_filename = shift @ARGV || "cat_$input_filename";
 
-print "in>>" . $input_filename . "\n";
-print "out<<" . $output_filename . "\n"; 
+print "input file: " . $input_filename . "\n";
+print "output file: " . $output_filename . "\n"; 
 
+# по умолчанию разделитель - знак табуляции
+my $sep = "\t";
+print "separator: tab\n";
 # чтобы построить розу ветров, нужно перевести градусы в стороны горизонта следующим образом:
 # тут более полно о румбах https://ru.wikipedia.org/wiki/%D0%A0%D1%83%D0%BC%D0%B1
 # СВ NE - от 22,5° до 67,5°
@@ -30,46 +33,63 @@ print "out<<" . $output_filename . "\n";
 # З W - от 247,5° до 292,5°
 # СЗ NW - от 292,5° до 337,5°
 # С N - от 337,5° до 360° и от 0° до 22,5°.
+
 # 02.09.15;13:57;26,1;45;13,0;2,0;2,5;135;0,0;0,0;995,0;0,0;23,8;47;2,5;26,1;26,1;3;0;0,00;0,00;25,7;646;0,0;135;0,0;0,0
 # 25 полей разделённых ;
-open FIN, "<$input_filename"
+# 
+my $fin;
+my $fout;
+open $fin, "<$input_filename"
 	or die "Can't open $input_filename: $!";
-open FOUT, ">$output_filename"
+open $fout, ">$output_filename"
 	or die "Can't open $output_filename: $!";
 map { chomp;
 	# выбрать данные удовлетворяющие шаблону
 	if (m/(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+?);(.+)/g) {
-		# категоризировать значение румба
+		# получить значение румба	
 		my $rumb = $8;
+		# установить категорию как неизвестную на всякий случай
+		my $rumb_cat = "?";
+		# заменить разделители ; и , на $sep(выбранный разделитель, по умолчанию таблуляция \t)
+		s/[;,]/$sep/gi;
+		
+		# категоризировать значение румба
 		if (22 <= $rumb && $rumb < 67) {
 		# СВ NE - от 22,5° до 67,5°
-			print FOUT "NE;$_\n";	
+			$rumb_cat = "NE";
 		} elsif (67 <= $rumb && $rumb < 112) {
 		# В E - от 67,5° до 112,5°
-			print FOUT "E;$_\n";	
+			$rumb_cat = "E";	
 		} elsif (112 <= $rumb && $rumb < 157) {
 		# ЮВ SE - от 112,5° до 157,5°
-			print FOUT "SE;$_\n";	
+			$rumb_cat = "SE";	
 		} elsif (157 <= $rumb && $rumb < 202) {
 		# Ю S - от 157,5° до 202,5°
-			print FOUT "S;$_\n";	
+			$rumb_cat = "S";	
 		} elsif (202 <= $rumb && $rumb < 247) {
 		# ЮЗ SW - от 202,5° до 247,5°
-			print FOUT "SW;$_\n";	
+			$rumb_cat = "SW";	
 		} elsif (247 <= $rumb && $rumb < 292) {
 		# З W - от 247,5° до 292,5°
-			print FOUT "W;$_\n";	
+			$rumb_cat = "W";	
 		} elsif (292 <= $rumb && $rumb < 337) {
 		# СЗ NW - от 292,5° до 337,5°
-			print FOUT "NW;$_\n";	
+			$rumb_cat = "NW";	
 		} elsif (337 <= $rumb && $rumb < 360) {
 		# С N - от 337,5° до 360° 
-			print FOUT "N;$_\n";	
+			$rumb_cat = "N";	
 		} elsif (0 <= $rumb && $rumb < 22) { 
 		# и от 0° до 22,5°.
-			print FOUT "N;$_\n";	
-		} #else {
-			#print FOUT "?;$_\n";
-		#}
+			$rumb_cat = "N";	
+		} 
+		# вывести преобразованную строку	
+		print $fout  $rumb_cat . $sep . $_ . "\n";	
+	
 	} # eof if
-} <FIN>;
+} <$fin>;
+
+close $fin
+	or die "Can't close $input_filename: $!";
+close $fout
+	or die "Can't close $output_filename: $!";
+ 
